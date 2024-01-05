147807f0cee601dd1c8735fdfd137d8d5f97cfa6
Mauro Fassi <maurofassi17@gmail.com>
2024-01-05 09:12:42 -0300

    creando la primer migracion

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layout/plantilla');
+
+@section('tituloPagina','Laravel Crud ')
+
+@section('contenido')
+<h1 class="display-1">Hola desde blade <i class="fa-solid fa-camera-retro"></i></h1>
+
+@endsection
